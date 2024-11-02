e45ffd910ca544a2c65a7d5957b4b9c499838590
Kevin Dang <kevin.dang@sheridancollege.ca>
2024-11-01 22:21:16 -0400

    Added page navigation

---
+++ frontend/newgame.js
@@ -0,0 +1,5 @@
+document.getElementById('start-game').addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log('start game');
+    window.location.href = './chessgame.html';
+});
